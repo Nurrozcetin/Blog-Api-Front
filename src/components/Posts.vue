65d45587fe929368824287c6ccb2188a85ae6402
Fatmanur Özçetin <93328444+Nurrozcetin@users.noreply.github.com>
2023-03-19 21:07:54 +0300

    create posts component

---
+++ src/components/Posts.vue
@@ -0,0 +1,8 @@
+<template>
+    <div class="posts">
+        <div class="post">
+            <h1 class="post-title">deneme başlık!</h1>
+            <p class="post-text">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Delectus amet voluptas eaque quis numquam assumenda voluptatibus suscipit, iure omnis porro ducimus expedita quisquam eius tempore recusandae debitis est aliquam vero?</p>     
+        </div>
+    </div>
+</template> 
